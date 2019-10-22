a506f0ab7b143e87dba9a2887bcb97421b7c30fc
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-22 17:32:18 +1100

    first setup for multer image uploads

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
+const fs = require('fs');
 const cors = require('cors');
 const app = express();
 
@@ -12,10 +14,28 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
+app.use(multer({ dest: "./uploads/",
+  rename: function (fieldname, filename) {
+    return filename;
+  },
+}));
+
 mongoose.connect(process.env.PRODUCTION_DB, {useUnifiedTopology: true, useNewUrlParser: true}, (err, success) => {
   if (err) { return console.error(err)}
   console.log('Connection Status: Success ✅');
 })
+
+const Post = require('./models/Post')
+app.post("/api/photo", async (req, res) => {
+  var post = new Post({
+    img: {
+      data: fs.readFileSync(req.files.userPhoto.path),
+      contentType: "image/png"
+    }
+  });
+  await newItem.save();
+  res.send(post);
+});
 
 app.get('/', (req, res, next) => {
   res.send('Hello World!');
