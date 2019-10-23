9c4d5edc7e28eb2c947ba3fd85565134a7a4b3c4
Chris Staudinger <chris.j.staudinger@gmail.com>
2019-10-23 11:43:37 +1100

    create user route is working. Atlas connection tested - working. login route working.

--- routes/posts.js
+++ routes/posts.js
@@ -11,6 +11,15 @@ router.get('/', async (req, res, next) => {
   }
 })
 
+// View user posts route
+router.get('/:user_id/:post_id', async (req, res, next) => {
+  try {
+    res.send("I am working 👍")
+  } catch (error) {
+    res.status(500).send(error)
+  }
+})
+
 router.get('/:id', async (req, res, next) => {
   try {
     res.send("I am working 👍")
--- routes/users.js
+++ routes/users.js
@@ -3,25 +3,42 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const User = require('./../models/User')
 
-router.get('/login', async (req, res, next) => {
-  try {
-    res.send("I am working 👍")
-  } catch (error) {
-   res.status(500).send(error)
-  }
-})
 
-// View user posts route
-router.get('/:id/:id', async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
-    res.send("I am working 👍")
+    res.send("I am working 👍👍")
   } catch (error) {
     res.status(500).send(error)
   }
 })
 
-router.get('/:id', async (req, res, next) => {
+router.post('/login', async (req, res, next) => {
+
+  const { email, password } = req.body
   try {
+    const user = await User.findOne({
+      email: email
+    })
+    if (user.password === password) {
+      res.send(user.id)
+    } else {
+      res.status(403).send("Password incorrect")
+    }
+  } catch (error) {
+   res.status(500).send(error)
+  }
+})
+
+router.post('/new', async (req, res, next) => {
+  try {
+    const { name, email, password } = req.body
+    const newUser = await User.create({
+      name,
+      email,
+      password
+    })
+    console.log(newUser)
+
     res.send("I am working 👍👍")
   } catch (error) {
     res.status(500).send(error)
