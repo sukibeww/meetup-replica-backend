7bb4dbdb1ff7b4fb94202341bca4a8372de0f63c
PushBtnsDev <kyledchenoweth@gmail.com>
2019-10-23 14:23:47 +1100

    registration validation complete

--- routes/users.js
+++ routes/users.js
@@ -30,18 +30,54 @@ router.post('/login', async (req, res, next) => {
 })
 
 router.post('/new', async (req, res, next) => {
-  try {
-    const { name, email, password } = req.body
-    const newUser = await User.create({
-      name,
-      email,
-      password
-    })
-    console.log(newUser)
+  const { name, email, password, password2 } = req.body
+  let errors = []
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({msg: 'Please fill in all fields'})
+  }
 
-    res.send("I am working 👍👍")
-  } catch (error) {
-    res.status(500).send(error)
+  // Check passwords match
+  if (password != password2) {
+    errors.push({msg:"Passwords do not match"})
+  }
+
+  // Check password length
+  if (password.length < 6) {
+    errors.push({msg: "Password needs to be at least 6 characters"})
+  }
+
+  if(errors.length > 0) {
+    res.send({
+      errors,
+      name,
+      email
+    })
+  } else {
+    // Validation Passed
+    try {
+      const user = await User.findOne({email : email})
+      if (user) {
+        // User exists - throw error
+        errors.push({msg: 'User already exists'})
+        res.send({
+          errors,
+          name,
+          email
+        })
+      } else {
+        const newUser = new User({
+          name: name,
+          email: email,
+          password: password
+        })
+        console.log('User added')
+        await newUser.save();
+        res.send(newUser.id)
+      }
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
 })
 
